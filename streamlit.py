4d66c36877a4e15f2a215ebca77a8cf6428d1272
SE-Nie <svenja.niehus@gmail.com>
2023-10-11 10:52:19 +0200

    predcition probability

--- streamlit.py
+++ streamlit.py
@@ -4,3 +4,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 st.title('Prediction of maliciousness of websites')    
+st.write("upload your website metadata to get a prediction of it's maliciousness")
+url = st.text_input('Enter the URL of the website you want to check', "URL")
+st.write('The URL you entered is: ', url)
+
+st.selectbox("Please select the Charset:",("Utf-8", "Iso-8859-1", "Windows-1252"))
+st.selectbox("Please select the country:",("US", "CA", "GB", "None"))
+
+for i in dict:
+    st.slider(i, min_value=None, max_value=None)
+    
+st.button('Predict')
